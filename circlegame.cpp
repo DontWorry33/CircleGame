442922d94087d73bd1024b294fd93f2ad2277cba
Hussain <hussain.hassam21@gmail.com>
2014-05-28 15:44:05 -0700

    added rotations for left and right movement

--- circlegame.cpp
+++ circlegame.cpp
@@ -19,11 +19,25 @@ int main()
     sf::Sprite bkg;
     bkg.setTexture(bkg_img);
 
+    //create texture for circle
+    sf::Texture shape_img;
+    shape_img.loadFromFile("temp_circle_texture.png");
 
+    //create circle and set texture
     sf::CircleShape shape(20.f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(460,555);
+    shape.setTexture(&shape_img);
 
+    //set origin coords of circle to radius=20 for good rotation
+    int origin_x = 20;
+    int origin_y = 20;
+
+    //set origin of circle to coords.
+    shape.setOrigin(origin_x,origin_y);
+
+    //set original position
+    shape.setPosition(460+origin_x,555+origin_y);
+
+    //x and y coords of circle
     int shape_x;
     int shape_y;
 
@@ -49,30 +63,38 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
 			//checks collisions and does nothing if it is colliding
-			if ((shape_x <= 235 && shape_y >= 400) || //portal box
-			   ((shape_x <= 590 && shape_x > 530) && shape_y >= 370)  ||  //line
-			    (shape_x <= 0)) NULL; //left side of screen
+			if ((shape_x <= 235+origin_x && shape_y >= 400+origin_y) || //portal box
+			   ((shape_x <= 590+origin_x && shape_x > 530+origin_x) && shape_y >= 370+origin_y)  ||  //line
+			    (shape_x <= 0+origin_x)) NULL; //left side of screen
 
-			//if no collisions, then move
-        		else shape.move(-5.f,0.f);
+			//if no collisions, then move and rotate
+        	else
+            {
+                shape.move(-5.f,0.f);
+                shape.rotate(-5.f);
+            } 
 		}
 
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
 			//checks collisions and does nothing if it is colliding
-			if (((shape_x >= 530 && shape_x < 590) && shape_y >= 370)  || //line 
-				 (shape_x+40 >= 800)) NULL; //right side of screen
+			if (((shape_x >= 530+origin_x && shape_x < 590+origin_x) && shape_y >= 370+origin_y)  || //line 
+				 (shape_x+40 >= 800+origin_x)) NULL; //right side of screen
 
-			//if no collisions, then move
-			else shape.move(5.f,0.f);
+			//if no collisions, then move and rotate
+			else 
+            {
+                shape.move(5.f,0.f);
+                shape.rotate(5.f);
+            }
 		}
 
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
 			//checks collisions and does nothing if it is colliding
-			if (shape_y <= 0) NULL; //top of screen
+			if (shape_y <= 0+origin_y) NULL; //top of screen
 			
 			//if no collisions, then move
 			else shape.move(0.f,-5.f);
@@ -82,12 +104,12 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
 			//checks collisions and does nothing if it is colliding
-			if  ((shape_x+5 <= 235 && shape_y >=395) || //top of portal box
-				 (shape_y+40 >= 595) ||  //bottom of screen
-				((shape_x > 530 && shape_x < 590) && shape_y >= 365)) NULL; //top of line
+			if  ((shape_x+5 <= 235+origin_x && shape_y >=395+origin_y) || //top of portal box
+				 (shape_y+40 >= 595+origin_y) ||  //bottom of screen
+				((shape_x > 530+origin_x && shape_x < 590+origin_x) && shape_y >= 365+origin_y)) NULL; //top of line
 
 			//if no collisions, then move
-            		else shape.move(0.f,5.f);
+            else shape.move(0.f,5.f);
 		}
 
 
