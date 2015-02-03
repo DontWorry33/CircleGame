5c1beaf57239d53a6dd5d04fd357d3032459845a
Dedie Kanda <dolente@live.com>
2015-02-02 20:48:51 -0500

    Shoot value changed

--- Coding/Bound.cpp
+++ Coding/Bound.cpp
@@ -226,7 +226,7 @@ Game::Game() :
 	mArrow.setOrigin(45,55);
 
 	//set powergauge stuff
-	mPowerGaugeMetreTexture.loadFromFile("../Character_Images/PowerGauge_Metre.png");
+	mPowerGaugeMetreTexture.loadFromFile("../Character_Images/PowerGauge_Metre2.png");
 	mPowerGaugeShellTexture.loadFromFile("../Character_Images/PowerGauge_Shell.png");
 	mPowerGaugeMetre.setTexture(&mPowerGaugeMetreTexture);
 	mPowerGaugeShell.setTexture(mPowerGaugeShellTexture);
@@ -1247,7 +1247,7 @@ void Game::powerMetreUpdate(sf::Keyboard::Key key)
 	if (key == sf::Keyboard::Space) 
 		{
 			mDrawMetre = true;
-			if (powerMetre <= 1) powerMetre+=0.03;
+			if (powerMetre <= 1) powerMetre+=0.075;
 			else powerMetre = 0;
 		}
 	else 
